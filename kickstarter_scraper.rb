d90c015366454aa8db848713a95667a7c14bec13
Richard Kurwitz <kurwitz3@aol.com>
2020-09-04 01:08:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,4 +11,5 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 #project.css("span.location-name").text
+#
 create_project_hash
