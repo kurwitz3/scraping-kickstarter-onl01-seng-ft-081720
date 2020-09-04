e54a1256919c20c00eff6cd9d4666c5d85a7c433
Richard Kurwitz <kurwitz3@aol.com>
2020-09-04 01:15:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,12 @@ kickstarter = Nokogiri::HTML(html)
 projects ={}
 
   kickstarter.css("li.project.grid_4").each do |project|
+     title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
  
+  # return the projects hash
+  projects
 end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
